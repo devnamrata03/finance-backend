09047c0bd2cabc1a3a1c531411d4ff61c2dca552
Devnamrata03 <Namratakumari9887@gmail.com>
2026-04-02 21:07:34 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.database import Base
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    type = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    date = Column(String, nullable=False)
+    notes = Column(String)
